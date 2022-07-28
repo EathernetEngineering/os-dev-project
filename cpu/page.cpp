4da9748a150572e368f747a7ef85d8e2305c614e
Chloe Eather <chloeeather@outlook.com>
2022-07-28 19:40:01 +1000

    Removed unused variables from page.cpp. Changed some comments in page.cpp to be more accurate.

--- cpu/page.cpp
+++ cpu/page.cpp
@@ -5,13 +5,6 @@
 #include "klibc/stdlib.hpp"
 #include "klibc/kprint.hpp"
 
-/* BIOS SET */
-PageMapLevel4 *_pPml4;
-PageDirectoryPointerTable *_pPdpt;
-PageDirectory *_pPd;
-PageTable *_ppPt[11];
-/* END BIOS SET */
-
 PageMapLevel4 *_pPageMap;
 
 void pageFaultHandler(registers_t* r);
@@ -20,18 +13,9 @@ void setupPaging()
 {
 	kprint("Setting ou paging.\n");
 
-	registerInterruptHandler(14, pageFaultHandler);
-	_pPml4 = reinterpret_cast<PageMapLevel4 *>(0x1000);
-	_pPdpt = reinterpret_cast<PageDirectoryPointerTable *>(0x2000);
-	_pPd = reinterpret_cast<PageDirectory *>(0x3000);
+	registerInterruptHandler(exceptions::pageFaultException, pageFaultHandler);
 
-	// Map tables set by bootloader to kernel varables
-	for (uint32_t i = 1; i < 11; i++)
-	{
-		_ppPt[0] = reinterpret_cast<PageTable *>(0x4000 + 0x1000 * i);
-	}
-
-	// reset page table location to 7MiB, map 4GiB
+	// Create new page table at location 7MiB, map 4GiB
 	_pPageMap = reinterpret_cast<PageMapLevel4 *>(0x700000);
 	ZeroMemory(_pPageMap, 0x900000);
 	for (uint64_t i = 0; i < 4; i++)
@@ -59,6 +43,7 @@ void setupPaging()
 	_pPageMap->entries[0].value =
 		3ll | 0x701000ll;
 
+	// Use new page table
 	__asm__ volatile("movq %0, %%cr3" : : "r"(_pPageMap));
 }
 
